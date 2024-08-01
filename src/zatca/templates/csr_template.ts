db66ded1ae708f31bfd3d6e1b4668e6125deb57b
vinod-osv <vinod@onebankview.com>
2024-08-01 11:23:00 +0530

    added fixes

--- src/zatca/templates/csr_template.ts
+++ src/zatca/templates/csr_template.ts
@@ -22,7 +22,7 @@ req_extensions = v3_req
 #basicConstraints=CA:FALSE
 #keyUsage = digitalSignature, keyEncipherment
 # Production or Testing Template (TSTZATCA-Code-Signing - ZATCA-Code-Signing)
-1.3.6.1.4.1.311.20.2 = ASN1:UTF8String:SET_PRODUCTION_VALUE
+1.3.6.1.4.1.311.20.2 = ASN1:PRINTABLESTRING:SET_PRODUCTION_VALUE
 subjectAltName=dirName:dir_sect
 
 [ dir_sect ]
