ba13f99a3b086799a1cf11018cdd2a4f215489bc
vinod-osv <vinod@onebankview.com>
2024-11-07 13:10:28 +0530

    added fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./zatca/egs";
+export * from "./zatca/egstd";
 export * from "./zatca/ZATCASimplifiedTaxInvoice";
+export * from "./zatca/ZATCAStandardTaxInvoice";
 export { generatePhaseOneQR } from "./zatca/qr";
